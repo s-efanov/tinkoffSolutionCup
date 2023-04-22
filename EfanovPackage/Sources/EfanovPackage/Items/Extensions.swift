ad86593bf62ab97ce9a95646bca2bf00f7e1d359
Сергей Ефанов <efanovss@tutu.tech>
2023-04-22 17:13:20 +0300

    Цвет для hightlined кнопки

--- EfanovPackage/Sources/EfanovPackage/Items/Extensions.swift
+++ EfanovPackage/Sources/EfanovPackage/Items/Extensions.swift
@@ -33,6 +33,10 @@ public enum EfanovColor {
         UIColor(red: 0, green: 0.063, blue: 0.141, alpha: 0.03)
     }
     
+    public static var buttonHightlinedBackgroundColor: UIColor {
+        UIColor(red: 0, green: 0.063, blue: 0.141, alpha: 0.06)
+    }
+    
     public static var buttonTittleColor: UIColor {
         UIColor(red: 0.259, green: 0.545, blue: 0.976, alpha: 1)
     }
